d0a29b6f16aa0ff30aa61ec900d49305a0b4f774
Yehuda Hason <yehudaxt@gmail.com>
2026-05-04 17:56:51 +0300

    shadow

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -2,8 +2,8 @@ export default function Timer() {
   return (
     <div className="timer-container">
       <svg className="timer-svg" viewBox="0 0 200 200">
-        <circle className="bg" cx="100" cy="100" r="87" />
-        <circle className="progress" cx="100" cy="100" r="87" />
+        <circle className="bg" cx="100" cy="100" r="89" />
+        <circle className="progress" cx="100" cy="100" r="89" />
       </svg>
       <div className="text">17:59</div>
     </div>
